cb93d0b7e8aba08089381af77e787746db2a6c14
StarsShadow-dev <101425256+StarsShadow-dev@users.noreply.github.com>
2024-04-02 17:11:18 -0400

    added tests

--- ts/main.ts
+++ ts/main.ts
@@ -1,10 +1,14 @@
+// tsc -p ./tsconfig.json && node out/main.js test/main.ucl
+
 import { setUpBuiltin } from "./builtin";
 import { compileFile } from "./compiler";
 
 setUpBuiltin();
 
+const filePath = process.argv[2];
+
 try {
-	compileFile("test/main.ucl");
+	compileFile(filePath);
 } catch (error) {
 	if (error == "__do nothing__") {
 		// do nothing
